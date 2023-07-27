e0e2c951d4f6e7ca4a746dc119ed7bd0723e22d4
abishegsri <137195570+abishegsri@users.noreply.github.com>
2023-07-27 17:29:04 +0530

    Update VotingToken.sol

--- UpdatedImplementation/VotingToken.sol
+++ UpdatedImplementation/VotingToken.sol
@@ -1,28 +1,36 @@
 pragma solidity ^0.8.14;
+
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-contract VotingToken is ERC20
-{
-      uint private immutable  maximum_supply;
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol"; // Import SafeERC20
+
+contract VotingToken is ERC20 {
+    using SafeERC20 for IERC20; // Add SafeERC20 library usage
+
+    uint private immutable maximum_supply;
     uint public immutable priceForToken;
-    constructor(string memory name,string memory symbol,uint max_supply,uint price)  ERC20(name, symbol)
-    {
-        maximum_supply=max_supply;
-        priceForToken=price;
-    }
-      function buyToken(uint _amount) public payable 
-    {
-        require((totalSupply()+_amount)<=maximum_supply,"a");
-        require(msg.value>=(_amount*priceForToken),"b");
-        _mint(msg.sender, _amount);
-    }
-    function stack_token(address ad,uint token_Stacked) public 
-    {
-        _transfer(ad, address(this),token_Stacked);
-    }
-   function unstack_token(address ad,uint token_Stacked) public 
-    {
-        _transfer(address(this),ad,token_Stacked);
+
+    constructor(string memory name, string memory symbol, uint max_supply, uint price) ERC20(name, symbol) {
+        maximum_supply = max_supply;
+        priceForToken = price;
     }
 
+    function buyToken(uint _amount) public payable {
+        require((totalSupply() + _amount) <= maximum_supply, "a");
+        require(msg.value >= (_amount * priceForToken), "b");
+        _mint(msg.sender, _amount);
+    }
+
+    function stack_token(address ad, uint token_Stacked) public {
+        _safeTransfer(msg.sender, address(this), token_Stacked); // Use safeTransfer
+    }
+
+    function unstack_token(address ad, uint token_Stacked) public {
+        _safeTransfer(address(this), msg.sender, token_Stacked); // Use safeTransfer
+    }
+
+    // Safe transfer function using SafeERC20
+    function _safeTransfer(address from, address to, uint256 amount) internal {
+        IERC20(this).safeTransferFrom(from, to, amount);
+    }
 }
